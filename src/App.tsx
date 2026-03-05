44b5f16e8daca82655d3b4b93eb9f5fdd8223a19
hzokbe <hzokbe@gmail.com>
2026-03-04 22:12:18 -0300

    feat: add theme toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
+import DarkMode from './assets/dark-mode.svg?react';
+import LightMode from './assets/light-mode.svg?react';
+import Button from './components/button/Button';
 import Card from './components/card/Card';
 import Page from './components/page/Page';
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = useState(
+    () =>
+      localStorage.getItem('theme') === 'dark' ||
+      document.documentElement.getAttribute('theme') === 'dark',
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      'theme',
+      darkTheme ? 'dark' : 'light',
+    );
+
+    localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
+  }, [darkTheme]);
+
   return (
     <Page>
       <Card>
         <h1>Tenki</h1>
       </Card>
+
+      <Button
+        className="themeToggleButton"
+        onClick={() => setDarkTheme((prev) => !prev)}
+      >
+        {darkTheme ? <LightMode /> : <DarkMode />}
+      </Button>
     </Page>
   );
 }
