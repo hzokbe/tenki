808e736438a12d90ac9ca3af89cfadaa2150297e
hzokbe <hzokbe@gmail.com>
2026-03-05 16:17:33 -0300

    feat: detect user location with loading and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import Button from './components/button/Button';
 import Card from './components/card/Card';
 import Page from './components/page/Page';
 
+type Status = 'LOADING' | 'SUCCESS' | 'ERROR';
+
 export default function App() {
   const [darkTheme, setDarkTheme] = useState(
     () =>
       localStorage.getItem('theme') === 'dark' ||
       document.documentElement.getAttribute('theme') === 'dark',
   );
+
+  const [status, setStatus] = useState<Status>('LOADING');
 
   useEffect(() => {
     document.documentElement.setAttribute(
@@ -21,6 +25,52 @@ export default function App() {
 
     localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
   }, [darkTheme]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      () => {
+        setStatus('SUCCESS');
+      },
+      () => setStatus('ERROR'),
+      {
+        enableHighAccuracy: false,
+        timeout: 10_000,
+        maximumAge: 300_000,
+      },
+    );
+  }, []);
+
+  if (status == 'LOADING') {
+    return (
+      <Page>
+        <h2>Getting your location...</h2>
+
+        <Button
+          className="themeToggleButton"
+          onClick={() => setDarkTheme((prev) => !prev)}
+        >
+          {darkTheme ? <LightMode /> : <DarkMode />}
+        </Button>
+      </Page>
+    );
+  }
+
+  if (status == 'ERROR') {
+    return (
+      <Page>
+        <h2>Unable to determine your location.</h2>
+
+        <p>Please check your GPS or internet connection.</p>
+
+        <Button
+          className="themeToggleButton"
+          onClick={() => setDarkTheme((prev) => !prev)}
+        >
+          {darkTheme ? <LightMode /> : <DarkMode />}
+        </Button>
+      </Page>
+    );
+  }
 
   return (
     <Page>
